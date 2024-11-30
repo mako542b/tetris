802a63e2484285d319b1143fe60e018af3468f12
mako542b <marcin.piotr.konopka@gmail.com>
2024-11-30 15:10:49 +0100

    changed block members initial values

--- include/SubBlocks.hpp
+++ include/SubBlocks.hpp
@@ -9,9 +9,9 @@ public:
     IBlock()
     {
         m_positions.reserve(2);
-        m_positions.push_back({position{0, 0}, position{0, 1}, position{0, 2}, position{0, 3}});
-        m_positions.push_back({position{0, 1}, position{1, 1}, position{2, 1}, position{3, 1}});
-        m_offsetX = 5;
+        m_positions.push_back({position{1, -1}, position{1, 0}, position{1, 1}, position{1, 2}});
+        m_positions.push_back({position{0, 0}, position{1, 0}, position{2, 0}, position{3, 0}});
+        m_offsetX = 6;
         m_offsetY = 0;
         m_blockID = BlockID::I_BLOCK;
     }
